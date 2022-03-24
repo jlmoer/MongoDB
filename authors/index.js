b1413db503fd83595f779eb1ed61123a285ab894
jlmoer <jlmoerdijk@gmail.com>
2022-03-24 11:48:15 +0100

    Authors finished

--- authors/index.js
+++ authors/index.js
@@ -21,12 +21,12 @@ mongoose
     .then(() => console.log("Connected to MongoDB"));
 
 
-// Exercice 1
+// Proof of connection to the database:
 app.get("/", (_req, res) => {
     res.send("Welcome to MongoDB");
 })
 
-// Exercice 2
+// Get an author by ID:
 app.get("/authors/:authorsId", async (req, res) => {
     console.log(req.params.authorsId);
     const authors = await Author.findById(req.params.authorsId);
@@ -34,6 +34,7 @@ app.get("/authors/:authorsId", async (req, res) => {
     res.json(authors);
 })
 
+// Add an author to the database:
 app.post("/authors", async (req, res) => {
     await Author.create(req.body);
 
@@ -41,34 +42,30 @@ app.post("/authors", async (req, res) => {
         message: "Author created",
     });
 });
-// Exercice 3
-app.get("/authors/:authorsId/books", (req, res) => {
-    const author = authors.find((author) => {
-        return author.id.toString() === req.params.authorsId;
-    })
-    // console.log(author);
-    res.send(`${author.books.join(", ")}`);
-})
 
-// Exercice 4
-app.get("/json/authors/:id", (req, res) => {
-    const author = authors.find((author) => {
-        return author.id.toString() === req.params.id;
-    })
-    res.json({
-        name: author.name,
-        nationality: author.nationality,
-    })
-})
+// Get all the books written by an author:
+app.get("/authors/:authorsId/books", async (req, res) => {
+    const author = await Author.findOne({ _id: req.params.authorsId }).select("-_id books");
+    res.send(author);
+});
 
-app.get("/json/authors/:id/books", (req, res) => {
-    const author = authors.find((author) => {
-        return author.id.toString() === req.params.id;
-    })
-    res.json({
-        books: author.books,
-    })
-})
+// // Get an author by ID with json:
+// app.get("/json/authors/:id", async (req, res) => {
+//     const author = await Author.findById({ _id: req.params.authorsId }).select("_id");
+//     res.json({
+//         name: author.name,
+//         nationality: author.nationality,
+//     })
+// })
+
+// app.get("/json/authors/:id/books", (req, res) => {
+//     const author = authors.find((author) => {
+//         return author.id.toString() === req.params.id;
+//     })
+//     res.json({
+//         books: author.books,
+//     })
+// });
 
 
 
